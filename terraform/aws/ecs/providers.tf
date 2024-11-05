6c7a41f1c90eccbd151dae903989bf28d985ae48
inflection-sahil <sahil.phule@inflectionzone.com>
2024-11-05 18:37:09 +0530

    added providers.tf file for aws

--- terraform/aws/ecs/providers.tf
+++ terraform/aws/ecs/providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 2.20.0"
+      version = "~> 3.0.2"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.64"
+      version = "~> 5.73"
     }
   }
 }
@@ -15,6 +15,6 @@ provider "docker" {}
 
 provider "aws" {
   region = local.aws_region
-  // shared_config_files = ["~/.aws/config"]
+  # shared_config_files = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
 }
--- terraform/aws/eks/providers.tf
+++ terraform/aws/eks/providers.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.64"
+      version = "~> 5.73"
     }
   }
 }
 
 provider "aws" {
   region = local.aws_region
-  // shared_config_files = ["~/.aws/config"]
+  # shared_config_files = ["~/.aws/config"]
   shared_credentials_files = ["~/.aws/credentials"]
 }
